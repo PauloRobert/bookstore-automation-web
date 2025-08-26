22d3c464c39f108846a0dd8aeccec79bbe5a8d27
Paulo Roberto <evans_sp@hotmail.com>
2025-08-26 18:48:02 -0300

    Apanhando do ConfigReader

--- src/test/java/com/demoqa/utils/ConfigReader.java
+++ src/test/java/com/demoqa/utils/ConfigReader.java
@@ -1,7 +1,7 @@
 package com.demoqa.utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
@@ -9,9 +9,11 @@ public class ConfigReader {
     private static Properties properties = new Properties();
 
     static {
-        try {
-            FileInputStream fis = new FileInputStream("config.properties");
-            properties.load(fis);
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Arquivo config.properties não encontrado no classpath!");
+            }
+            properties.load(input);
         } catch (IOException e) {
             throw new RuntimeException("Erro ao carregar config.properties", e);
         }

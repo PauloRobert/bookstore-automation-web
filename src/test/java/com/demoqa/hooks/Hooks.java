8913afc221fa2c2e59bf12e27d70892309cd2f20
Paulo Roberto <evans_sp@hotmail.com>
2025-08-26 18:23:22 -0300

    Include Browsers

--- src/test/java/com/demoqa/hooks/Hooks.java
+++ src/test/java/com/demoqa/hooks/Hooks.java
@@ -1,5 +1,6 @@
 package com.demoqa.hooks;
 
+import com.demoqa.utils.ConfigReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -7,13 +8,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 /**
- * Classe responsável por iniciar e fechar o navegador.
+ * Classe de inicialização e finalização do navegador.
  * Também captura screenshots em falha ou sucesso.
  */
 public class Hooks {
@@ -22,8 +27,41 @@ public class Hooks {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        String browser = ConfigReader.getProperty("browser").toLowerCase();
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            case "firefox-headless":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions ffOptions = new FirefoxOptions();
+                ffOptions.addArguments("--headless");
+                driver = new FirefoxDriver(ffOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            case "chrome-headless":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chOptions = new ChromeOptions();
+                chOptions.addArguments("--headless=new");
+                chOptions.addArguments("--window-size=1920,1080");
+                driver = new ChromeDriver(chOptions);
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+
         driver.manage().window().maximize();
     }
 

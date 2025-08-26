b0af172ed3e559b74cbadff56c69713978e33b3b
Paulo Roberto <evans_sp@hotmail.com>
2025-08-26 18:24:24 -0300

    Delete HardCode

--- src/test/java/com/demoqa/steps/FormSteps.java
+++ src/test/java/com/demoqa/steps/FormSteps.java
@@ -2,6 +2,7 @@ package com.demoqa.steps;
 
 import com.demoqa.hooks.Hooks;
 import com.demoqa.pages.FormPage;
+import com.demoqa.utils.ConfigReader;
 import io.cucumber.java.pt.*;
 
 public class FormSteps {
@@ -10,18 +11,19 @@ public class FormSteps {
 
     @Dado("que o usuário navega até o site")
     public void que_o_usuario_navega_ate_o_site() {
-        Hooks.driver.get("https://demoqa.com/");
+        String url = ConfigReader.getProperty("url");
+        Hooks.driver.get(url);
+        formPage = new FormPage(Hooks.driver);
     }
 
     @Quando("clica no cartão Forms")
     public void clica_no_cartao_forms() {
-        Hooks.driver.findElement(org.openqa.selenium.By.xpath("//h5[text()='Forms']")).click();
+        formPage.acessarForms();
     }
 
     @Quando("seleciona a opção Practice Form")
     public void seleciona_a_opcao_practice_form() {
-        Hooks.driver.findElement(org.openqa.selenium.By.xpath("//span[text()='Practice Form']")).click();
-        formPage = new FormPage(Hooks.driver);
+        formPage.acessarPracticeForm();
     }
 
     @Quando("preenche todos os campos com dados válidos")
